dc2fc28351ac0b56c796e2c2c652134d85751a3b
ahmet pehlivan <115579325+ahmet-pehlivan@users.noreply.github.com>
2023-04-12 02:44:37 +0300

    Initial commit

---
+++ UnitOfWorkDemo.Infrastructure/DbContextClass.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UnitOfWorkDemo.Core.Models;
+
+namespace UnitOfWorkDemo.Infrastructure
+{
+    public class DbContextClass : DbContext
+    {
+        public DbContextClass(DbContextOptions<DbContextClass> contextOptions) : base(contextOptions)
+        {
+
+        }
+
+        public DbSet<ProductDetails> Products { get; set; }
+    }
+}
